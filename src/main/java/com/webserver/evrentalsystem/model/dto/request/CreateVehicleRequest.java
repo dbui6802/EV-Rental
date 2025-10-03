8c9af4644ff371e29501db9be94ddcd6c28e1ef3
VuDuc219 <nivuduc2191999@gmail.com>
2025-10-03 10:06:27 +0000

    update vehicle vs user

---
+++ src/main/java/com/webserver/evrentalsystem/model/dto/request/CreateVehicleRequest.java
@@ -0,0 +1,41 @@
+package com.webserver.evrentalsystem.model.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateVehicleRequest {
+
+    @Schema(description = "Biển số xe", example = "30A-12345")
+    private String licensePlate;
+
+    @Schema(description = "Loại xe", example = "car", allowableValues = {"car", "motorbike"})
+    private String type;
+
+    @Schema(description = "Hãng xe", example = "Toyota")
+    private String brand;
+
+    @Schema(description = "Mẫu xe", example = "Vios")
+    private String model;
+
+    @Schema(description = "Dung lượng pin (kWh)", example = "40")
+    private Integer capacity;
+
+    @Schema(description = "Quãng đường di chuyển trên mỗi lần sạc đầy (km)", example = "250")
+    private Integer rangePerFullCharge;
+
+    @Schema(description = "Trạng thái xe", example = "available", allowableValues = {"reserved", "available", "reserved", "maintenance", "rented"})
+    private String status;
+
+    @Schema(description = "Giá thuê mỗi giờ", example = "150000")
+    private BigDecimal pricePerHour;
+
+    @Schema(description = "ID trạm", example = "1")
+    private Long stationId;
+}
