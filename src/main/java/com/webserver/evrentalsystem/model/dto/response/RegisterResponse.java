df216d82b666df48a2c732a488aa6e152c3dcd28
VuDuc219 <nivuduc2191999@gmail.com>
2025-10-03 08:47:06 +0000

    update

---
+++ src/main/java/com/webserver/evrentalsystem/model/dto/response/RegisterResponse.java
@@ -0,0 +1,14 @@
+package com.webserver.evrentalsystem.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+    private String message;
+}
