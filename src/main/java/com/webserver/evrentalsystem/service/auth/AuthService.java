80533de8e808f8f03c0cdc01df65a0b92f6aaf8a
VuDuc219 <nivuduc2191999@gmail.com>
2025-10-04 15:27:38 +0000

    update

--- src/main/java/com/webserver/evrentalsystem/service/auth/AuthService.java
+++ src/main/java/com/webserver/evrentalsystem/service/auth/AuthService.java
@@ -1,11 +1,13 @@
-package com.webserver.evrentalsystem.service;
+package com.webserver.evrentalsystem.service.auth;
 
-import com.webserver.evrentalsystem.model.dto.SignInRequest;
-import com.webserver.evrentalsystem.model.dto.SignInResponse;
-import jakarta.servlet.http.HttpServletRequest;
+import com.webserver.evrentalsystem.model.dto.request.RegisterRequest;
+import com.webserver.evrentalsystem.model.dto.response.RegisterResponse;
+import com.webserver.evrentalsystem.model.dto.request.SignInRequest;
+import com.webserver.evrentalsystem.model.dto.response.SignInResponse;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
     SignInResponse signIn(SignInRequest signinRequest, HttpServletResponse httpServletResponse);
-    void signOut(HttpServletRequest request, HttpServletResponse response);
+    void signOut(HttpServletResponse response);
+    RegisterResponse register(RegisterRequest request, HttpServletResponse response);
 }
--- src/main/java/com/webserver/evrentalsystem/service/auth/impl/AuthServiceImpl.java
+++ src/main/java/com/webserver/evrentalsystem/service/auth/impl/AuthServiceImpl.java
@@ -1,17 +1,19 @@
-package com.webserver.evrentalsystem.service.impl;
+package com.webserver.evrentalsystem.service.auth.impl;
 
+import com.webserver.evrentalsystem.entity.Role;
 import com.webserver.evrentalsystem.entity.User;
 import com.webserver.evrentalsystem.exception.InvalidateParamsException;
 import com.webserver.evrentalsystem.exception.UserNotFoundException;
-import com.webserver.evrentalsystem.model.dto.SignInRequest;
-import com.webserver.evrentalsystem.model.dto.SignInResponse;
-import com.webserver.evrentalsystem.model.mapping.UserMapping;
+import com.webserver.evrentalsystem.model.dto.request.RegisterRequest;
+import com.webserver.evrentalsystem.model.dto.response.RegisterResponse;
+import com.webserver.evrentalsystem.model.dto.request.SignInRequest;
+import com.webserver.evrentalsystem.model.dto.response.SignInResponse;
+import com.webserver.evrentalsystem.model.mapping.UserMapper;
 import com.webserver.evrentalsystem.repository.UserRepository;
-import com.webserver.evrentalsystem.service.AuthService;
+import com.webserver.evrentalsystem.service.auth.AuthService;
 import com.webserver.evrentalsystem.service.validation.UserValidation;
 import com.webserver.evrentalsystem.utils.CookieUtils;
 import com.webserver.evrentalsystem.utils.JwtUtils;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -33,7 +35,10 @@ public class AuthServiceImpl implements AuthService {
     private UserRepository userRepository;
 
     @Autowired
-    private UserMapping userMapping;
+    private UserValidation userValidation;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -70,7 +75,9 @@ public class AuthServiceImpl implements AuthService {
         userRepository.save(user);
 
         SignInResponse signinResponse = new SignInResponse();
-        signinResponse.setUserInfo(userMapping.toUserDto(user));
+        signinResponse.setUserInfo(userMapper.toUserDto(user));
+        signinResponse.setAccessToken(accessToken);
+        signinResponse.setRefreshToken(refreshToken);
 
         // save access token to cookie
         ResponseCookie accessTokenCookie = CookieUtils.createCookie(CookieUtils.ACCESS_TOKEN, accessToken, (int) (jwtAccessTokenExpirationMs / 1000));
@@ -82,8 +89,8 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public void signOut(HttpServletRequest request, HttpServletResponse httpServletResponse) {
-        User user = UserValidation.validateUser(userRepository, request);
+    public void signOut(HttpServletResponse httpServletResponse) {
+        User user = userValidation.validateUser();
 
         user.setRefreshToken(null);
         userRepository.save(user);
@@ -93,5 +100,66 @@ public class AuthServiceImpl implements AuthService {
         ResponseCookie refreshTokenCookie = CookieUtils.createCookie(CookieUtils.REFRESH_TOKEN, null, 0);
         httpServletResponse.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
         httpServletResponse.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+    }
+
+    @Override
+    public RegisterResponse register(RegisterRequest request, HttpServletResponse response) {
+        String phone = request.getPhone();
+        String password = request.getPassword();
+        String fullName = request.getFullName();
+
+        if (phone.isEmpty() || password.isEmpty() || fullName.isEmpty()) {
+            throw new InvalidateParamsException("Số điện thoại, mật khẩu và tên không được để trống");
+        }
+
+        User existingUser = userRepository.findByPhone(phone);
+        if (existingUser != null) {
+            throw new InvalidateParamsException("Số điện thoại đã được đăng ký");
+        }
+
+        // validate Viet Nam phone number
+        String regex = "^(?:\\+84|0)(?:3[2-9]|5(?:6|8|9)|7(?:0|6|7|8|9)|8[1-9]|9[0-9])\\d{7}$";
+        if (!phone.matches(regex)) {
+            throw new InvalidateParamsException("Số điện thoại không hợp lệ");
+        }
+
+        // check strong password
+        if (password.length() < 8) {
+            throw new InvalidateParamsException("Mật khẩu phải có ít nhất 8 ký tự");
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            throw new InvalidateParamsException("Mật khẩu phải có ít nhất 1 ký tự in hoa");
+        }
+        if (!password.matches(".*[a-z].*")) {
+            throw new InvalidateParamsException("Mật khẩu phải có ít nhất 1 ký tự in thường");
+        }
+        if (!password.matches(".*\\d.*")) {
+            throw new InvalidateParamsException("Mật khẩu phải có ít nhất 1 chữ số");
+        }
+        if (!password.matches(".*[!@#$%^&*()].*")) {
+            throw new InvalidateParamsException("Mật khẩu phải có ít nhất 1 ký tự đặc biệt");
+        }
+
+        if (request.getEmail() != null && !request.getEmail().isEmpty()) {
+            // validate email
+            String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+            if (!request.getEmail().matches(emailRegex)) {
+                throw new InvalidateParamsException("Email không hợp lệ");
+            }
+        }
+
+        User newUser = new User();
+        newUser.setPhone(phone);
+        newUser.setPassword(passwordEncoder.encode(password));
+        newUser.setFullName(fullName);
+        newUser.setEmail(request.getEmail());
+        newUser.setRole(Role.RENTER); // default role is RENTER
+
+        userRepository.save(newUser);
+
+        RegisterResponse registerResponse = new RegisterResponse();
+        registerResponse.setMessage("Đăng ký thành công");
+
+        return registerResponse;
     }
 }
